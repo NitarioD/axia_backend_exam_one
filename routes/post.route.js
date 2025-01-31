a9b5d9692e7c43aeebd61f4c6d91a4215cec866d
Jola <nitariod@gmail.com>
2025-01-31 23:02:44 +0100

    fixed import authenticate issue

--- routes/post.route.js
+++ routes/post.route.js
@@ -1,15 +1,15 @@
 const express = require('express');
-const authenticate = require('../middleware/auth');
+const {authenticate} = require('../middleware/auth');
 
 const {createPost, deletePost, updatePost, getAllPosts, getPost } = require('../controllers/post.controller');
 
 const routes = express.Router();
 
-// routes.post('/post', authenticate, createPost);
-// routes.put('/post/:id', authenticate, updatePost);
-// routes.delete('/post/:id', authenticate, deletePost);
-// routes.get('/post', getAllPosts)
-// routes.get('/post/:id', getPost)
+routes.post('/post', authenticate, createPost);
+routes.put('/post/:id', authenticate, updatePost);
+routes.delete('/post/:id', authenticate, deletePost);
+routes.get('/post', getAllPosts)
+routes.get('/post/:id', getPost)
 
 
 module.exports = routes;
